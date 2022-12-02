d78a26a658dc145e914cc44c09a7537d44a4bab7
Alexandr Sanatar <allexvik@gmail.com>
2022-12-02 17:25:21 +0200

    upd

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const stars = 2;
-let price;
+// const stars = 2;
+// let price;
 
 // if (stars === 1) {
 //   price = 20;
@@ -15,38 +15,56 @@ let price;
 //   console.log("there is no such stars amount");
 // }
 
-switch (stars) {
-  case 1:
-    price = 20;
-    break;
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     break;
 
-  case 2:
-    price = 30;
-    break;
+//   case 2:
+//     price = 30;
+//     break;
 
-  case 3:
-    price = 50;
-    break;
+//   case 3:
+//     price = 50;
+//     break;
 
-  case 4:
-    price = 70;
-    break;
+//   case 4:
+//     price = 70;
+//     break;
 
-  case 5:
-    price = 120;
-    break;
+//   case 5:
+//     price = 120;
+//     break;
 
-  default:
-    console.log("there is no such stars amount");
+//   default:
+//     console.log("there is no such stars amount");
+// }
+
+// console.log(price);
+
+// const mass = [1, 2, 3, 6, 5, 4, 7, 8, 9];
+// let totalMass = 0;
+
+// for (const val of mass) {
+//   totalMass += val;
+// }
+
+// console.log("totalMass: ", totalMass);
+
+const loginMass = ["firstLogin", "secondLogin", "thirdLogin"];
+let loginToFind = "secondLogin";
+let message = "";
+
+for (let i = 0; i < loginMass.length; i += 1) {
+  const login = loginMass[i];
+
+  console.log("Login :", login);
+
+  if (login === loginToFind) {
+    message = `user ${loginToFind} has been found!`;
+    break;
+  }
+
+  message = `user ${loginToFind} has NOT been found :(`;
 }
-
-console.log(price);
-
-const mass = [1, 2, 3, 6, 5, 4, 7, 8, 9];
-let totalMass = 0;
-
-for (const val of mass) {
-  totalMass += val;
-}
-
-console.log("totalMass: ", totalMass);
+console.log(message);
